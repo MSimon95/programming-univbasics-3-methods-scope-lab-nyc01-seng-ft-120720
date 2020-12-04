c85a60d68a450d22e1a2d84fd02bff89914cae64
madiwa Simon <mad.simon01@gmail.com>
2020-12-04 16:56:01 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -15,7 +15,7 @@ phrase2 = "It's Dangerous To Go Alone! Take This."
 puts phrase2
 end
 
-def all_phrase
+def all_phrases
   
     phrase = "It's-a me, Mario!"
       status = 'Thank You Mario! But Our Princess Is In Another Castle!'
